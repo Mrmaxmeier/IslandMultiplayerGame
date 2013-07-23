5a1d5792f2a635845cd42a306bec0609511a742a
Mrmaxmeier <Mrmaxmeier@gmail.com>
2013-07-23 12:44:04 +0200

    try finally server

--- testserver.py
+++ testserver.py
@@ -7,14 +7,17 @@ def handle(sock, addr):
 		print addr, ":", sock.recv(1024)
 
 def serve(port, message):
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind((socket.gethostname(), port))
-	serversocket.listen(5)
-	while 1:
-		print "server ready!"
-		(clientsocket, address) = serversocket.accept()
-		clientsocket.send(message)
-		thread.start_new_thread(handle, (clientsocket, address))
+	try:
+		serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serversocket.bind((socket.gethostname(), port))
+		serversocket.listen(5)
+		while 1:
+			print "server ready!"
+			(clientsocket, address) = serversocket.accept()
+			clientsocket.send(message)
+			thread.start_new_thread(handle, (clientsocket, address))
+	finally:
+		serversocket.close()
 
 def testclient(addr):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
